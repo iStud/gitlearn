8231cf98128d5658be775d61440928e0da4f70b4
xiongbk <istud.k@gmail.com>
2023-04-18 08:51:53 +0800

    初始化

---
+++ gitLarn/gitLarn/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  gitLarn
+//
+//  Created by xbk on 2023/4/18.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
