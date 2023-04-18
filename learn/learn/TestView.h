51162dbbcee17cf056161be5ff4443a7db34cf68
xiongbk <istud.k@gmail.com>
2023-04-18 09:40:29 +0800

    add testview

---
+++ learn/learn/TestView.h
@@ -0,0 +1,16 @@
+//
+//  TestView.h
+//  learn
+//
+//  Created by xbk on 2023/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ learn/learn/TestView.m
@@ -0,0 +1,20 @@
+//
+//  TestView.m
+//  learn
+//
+//  Created by xbk on 2023/4/18.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
