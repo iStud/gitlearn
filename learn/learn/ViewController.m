a1ae01057599f56312b63c967859ce1e093bc30d
xiongbk <istud.k@gmail.com>
2023-04-18 09:33:43 +0800

    添加一个蓝色view

--- learn/learn/ViewController.m
+++ learn/learn/ViewController.m
@@ -16,6 +16,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(10, 50, 44, 44)];
+    [self.view addSubview:view];
+    view.backgroundColor = [UIColor blueColor];
+    
+    
 }
 
 
